cc70d557ff07fc7fb9ddb5f1e12bb86cb0f5b519
yoshida874 <>
2021-08-04 14:17:15 +0900

    tableにボタンを追加

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import * as moment from 'moment';
+
+
+const ELEMENT_DATA = [
+  {name: 'Hydrogen', birthDay: '2019-02-01', age: '2歳'},
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+  displayedColumns = ['名前', '誕生日', '年齢', 'ボタン'];
+  elementData = ELEMENT_DATA;
+  dataSource = new MatTableDataSource(this.elementData);
+  name = 'name';
+  birthDay = '';
+  age = 0;
+
+  pickerInput(){
+    // 年齢を算出する
+    this.age = moment().diff(this.birthDay, 'years');
+  }
+
+  add(){
+    // YYYY-MM-DD形式に変換する
+    const outputDay = moment(this.birthDay).format('YYYY-MM-DD');
+    const outputAge = `${this.age}歳`
+    this.elementData.push({name: this.name, birthDay: outputDay, age: outputAge});
+    // テーブル更新
+    this.dataSource = new MatTableDataSource(this.elementData);
+    this.dataSource.paginator = this.paginator;
+  }
+
+  delete(index: number){
+    // 削除
+    this.elementData.splice(index, 1);
+    this.dataSource = new MatTableDataSource(this.elementData);
+    this.dataSource.paginator = this.paginator;
+  }
+
+  @ViewChild(MatPaginator) paginator: any;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatListModule } from '@angular/material/list'; // 追加
+import { MatButtonModule } from '@angular/material/button'; // 追加
+import { MatInputModule } from '@angular/material/input'; // 追加
+import { MatToolbarModule } from '@angular/material/toolbar'; // 追加
+import { MatDividerModule } from '@angular/material/divider'; // 追加
+import { MatCardModule } from '@angular/material/card'; // 追加
+import { MatIconModule } from '@angular/material/icon'; // 追加
+
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { DeleteDialogComponent } from './components/delete-dialog/delete-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeleteDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule, // 追加
+    MatInputModule, // 追加
+    MatToolbarModule, // 追加
+    MatDividerModule, // 追加
+    MatCardModule, // 追加
+    MatIconModule, // 追加
+    MatListModule, // 追加
+    MatTableModule,
+    MatPaginatorModule,
+    MatDatepickerModule,
+    // MatNativeDateModule,
+    MatMomentDateModule,
   ],
-  providers: [],
+  providers: [{provide: MAT_DATE_LOCALE, useValue: 'ja-JP'},],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
